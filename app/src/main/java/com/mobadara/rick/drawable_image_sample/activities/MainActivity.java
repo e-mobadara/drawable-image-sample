c83ccb9884b5f73413f7336f67a36fc2b69245af
Mohammed slimani <slimani.med.21@gmail.com>
2019-04-15 18:31:57 +0100

    Added DrawableImage

---
+++ app/src/main/java/com/mobadara/rick/drawable_image_sample/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mobadara.selma.drawable_image_sample.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.emobadaragaminglib.Base.Screen;
+import com.example.emobadaragaminglib.Implementation.AndroidGame;
+import com.mobadara.selma.drawable_image_sample.R;
+
+public class MainActivity extends AndroidGame {
+
+    @Override
+    public Screen getInitScreen() {
+        return null;
+    }
+}
